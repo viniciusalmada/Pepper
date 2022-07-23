07cd1130ff6acc6c273575777bcf068eca7289e7
Vinicius Almada <viniciusalmada10@gmail.com>
2022-07-23 10:59:07 -0300

    create assertions macros

--- Butter/src/Butter/Core.hpp
+++ Butter/src/Butter/Core.hpp
@@ -9,3 +9,25 @@
 #else
   #error Butter only support Windows!
 #endif
+
+#ifdef BT_ENABLE_ASSERTS
+  #define BT_ASSERT(x, ...)                                                    \
+    {                                                                          \
+      if (!x)                                                                  \
+      {                                                                        \
+        BT_ERROR("Assertion failed: {0}", __VA_ARGS__);                        \
+        __debugbreak();                                                        \
+      }                                                                        \
+    }
+  #define BT_CORE_ASSERT(x, ...)                                               \
+    {                                                                          \
+      if (!x)                                                                  \
+      {                                                                        \
+        BT_CORE_ERROR("Assertion failed: {0}", __VA_ARGS__);                   \
+        __debugbreak();                                                        \
+      }                                                                        \
+    }
+#else
+  #define BT_ASSERT(x, ...)
+  #define BT_CORE_ASSERT(x, ...)
+#endif
