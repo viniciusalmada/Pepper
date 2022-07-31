15aea76f5ea712d80093e82dea12dd87f9ef1d2d
Vinicius <viniciusalmada10@gmail.com>
2022-07-31 15:45:47 -0300

    add glm include directories to Pepper and App

--- App/src/ClientApp.cpp
+++ App/src/ClientApp.cpp
@@ -1,10 +1,30 @@
+// clang-format off
 #include <Pepper.hpp>
 #include <PepperPCH.hpp>
+// clang-format on
+
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/vec3.hpp>   // glm::vec3
+#include <glm/vec4.hpp>   // glm::vec4
+glm::mat4 camera(float Translate, glm::vec2 const& Rotate)
+{
+  glm::mat4 Projection =
+    glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.f);
+  glm::mat4 View =
+    glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -Translate));
+  View = glm::rotate(View, Rotate.y, glm::vec3(-1.0f, 0.0f, 0.0f));
+  View = glm::rotate(View, Rotate.x, glm::vec3(0.0f, 1.0f, 0.0f));
+  glm::mat4 Model = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f));
+  return Projection * View * Model;
+}
 
 class ExampleLayer : public Pepper::Layer
 {
 public:
-  ExampleLayer() : Layer("Example") {}
+  ExampleLayer() : Layer("Example") {
+    auto a =  camera(5.0f, {1.0f, 2.0f});
+  }
 
   void OnUpdate() override
   {
@@ -12,7 +32,9 @@ public:
       PP_INFO("Tab pressed");
   }
 
-  void OnEvent(Pepper::Event& event) override { /* PP_TRACE("{0}", event); */ }
+  void OnEvent(Pepper::Event& event) override
+  { /* PP_TRACE("{0}", event); */
+  }
 };
 
 class ClientApp : public Pepper::Application
