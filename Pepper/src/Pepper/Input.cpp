6d24cb6026d428287afe264433e7da44f7fdf00c
Vinicius <viniciusalmada10@gmail.com>
2022-08-07 11:29:00 -0300

    remove inline from Input static functions

--- Pepper/src/Pepper/Input.cpp
+++ Pepper/src/Pepper/Input.cpp
@@ -1,15 +1,15 @@
 #include "Input.hpp"
 
-inline bool Pepper::Input::IsKeyPressed(PPKey keyCode)
+bool Pepper::Input::IsKeyPressed(PPKey keyCode)
 {
   return instance->IsKeyPressedImpl(keyCode);
 }
 
-inline bool Pepper::Input::IsMouseButtonPressed(PPMouseBt button)
+bool Pepper::Input::IsMouseButtonPressed(PPMouseBt button)
 {
   return instance->IsMouseButtonImpl(button);
 }
 
-inline float Pepper::Input::GetMouseX() { return instance->GetMouseXImpl(); }
+float Pepper::Input::GetMouseX() { return instance->GetMouseXImpl(); }
 
-inline float Pepper::Input::GetMouseY() { return instance->GetMouseYImpl(); }
+float Pepper::Input::GetMouseY() { return instance->GetMouseYImpl(); }
