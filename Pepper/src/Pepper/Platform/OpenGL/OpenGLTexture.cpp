2b2f991f0f103b5f053f0238de823c1c0e6aab91
Vinicius Almada <viniciusalmada10@gmail.com>
2022-08-20 11:51:05 -0300

    change texture settings on load

--- Pepper/src/Pepper/Platform/OpenGL/OpenGLTexture.cpp
+++ Pepper/src/Pepper/Platform/OpenGL/OpenGLTexture.cpp
@@ -13,6 +13,7 @@ Pepper::OpenGLTexture2D::OpenGLTexture2D(const std::string& path) : path(path)
 {
   int w{}, h{};
   int channels{};
+  stbi_set_flip_vertically_on_load(1);
   stbi_uc* data = stbi_load(path.c_str(), &w, &h, &channels, 0);
   PP_CORE_ASSERT(data, "Failed to load image!");
 
@@ -23,7 +24,7 @@ Pepper::OpenGLTexture2D::OpenGLTexture2D(const std::string& path) : path(path)
   glTextureStorage2D(renderer_ID, 1, GL_RGB8, width, height);
 
   glTextureParameteri(renderer_ID, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTextureParameteri(renderer_ID, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTextureParameteri(renderer_ID, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
   glTextureSubImage2D(renderer_ID, 0, 0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
 
