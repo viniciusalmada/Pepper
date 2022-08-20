54eb26536297e8ae129406def296cfda53ccd95b
Vinicius Almada <viniciusalmada10@gmail.com>
2022-08-20 14:37:38 -0300

    create an OpenGL debugger function

---
+++ Pepper/src/Pepper/Platform/OpenGL/OpenGLDebugger.cpp
@@ -0,0 +1,26 @@
+// clang-format off
+#include "PepperPCH.hpp"
+// clang-format on
+
+#include "OpenGLDebugger.hpp"
+
+#include <glad/glad.h>
+
+void Pepper::OpenGLDebugger::Func(GLenum /* source */,
+                                  GLenum /* type */,
+                                  unsigned int id,
+                                  GLenum /* severity */,
+                                  GLsizei /* length */,
+                                  const char* message,
+                                  const void* /* userParam */)
+{
+  // ignore non-significant error/warning codes
+  if (id == 131169 || id == 131185 || id == 131218 || id == 131204)
+    return;
+
+  PP_CORE_WARN("--------------------");
+  PP_CORE_WARN("OpenGL Error:");
+  PP_CORE_WARN("(0x{0:04x}): {1}", id, message);
+
+  PP_CORE_ASSERT(false, "");
+}
