583090dcf5e8725b225015018860388d594bad44
Vinicius Almada <viniciusalmada10@gmail.com>
2022-08-30 00:40:24 -0300

    extract Library from Shader header and use PIMPL

---
+++ Pepper/src/Pepper/Renderer/ShaderLibrary.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Pepper/Core/Core.hpp"
+#include "Shader.hpp"
+
+#include <filesystem>
+
+namespace Pepper
+{
+  class ShaderLibrary
+  {
+  public:
+    ShaderLibrary();
+    ~ShaderLibrary();
+
+    void Add(const Ref<Shader>&);
+    Ref<Shader> Load(const std::filesystem::path&);
+
+    Ref<Shader> Get(const std::string&) const;
+
+  private:
+    PIMPL()
+  };
+}
