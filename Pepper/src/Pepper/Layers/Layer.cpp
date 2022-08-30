8d290d542e402b068969a00cce3b7c370db629c8
Vinicius Almada <viniciusalmada10@gmail.com>
2022-08-29 23:54:51 -0300

    change Layer to use PIMPL

--- Pepper/src/Pepper/Layers/Layer.cpp
+++ Pepper/src/Pepper/Layers/Layer.cpp
@@ -6,5 +6,31 @@
 
 namespace Pepper
 {
-  Layer::Layer(const std::string& name) : debugName(name) {}
+  class Layer::Impl
+  {
+  public:
+    std::string debugName;
+  };
+
+  Layer::Layer(const std::string& name) : pimp(new Impl{})
+  {
+    pimp->debugName = name;
+  }
+
+  Layer::~Layer() = default;
+
+  void Layer::OnAttach() {}
+
+  void Layer::OnDetach() {}
+
+  void Layer::OnUpdate(TimeStep) {}
+
+  void Layer::OnImGuiRender() {}
+
+  void Layer::OnEvent(Event&) {}
+
+  const std::string& Layer::GetName() const
+  {
+    return pimp->debugName;
+  }
 }
