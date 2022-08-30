7a62949e61e98bb1a17ef26a0962f288ba219052
Vinicius Almada <viniciusalmada10@gmail.com>
2022-08-29 21:04:01 -0300

    define core macros to use inside Pepper only

--- Pepper/src/Pepper/Core/Log.hpp
+++ Pepper/src/Pepper/Core/Log.hpp
@@ -20,8 +20,8 @@ namespace Pepper
   public:
     static void Init();
 
-    inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return core_logger; }
-    inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return client_logger; }
+    static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return core_logger; }
+    static std::shared_ptr<spdlog::logger>& GetClientLogger() { return client_logger; }
 
   private:
     static std::shared_ptr<spdlog::logger> core_logger;
@@ -29,16 +29,18 @@ namespace Pepper
   };
 }
 
-// Core log macros
-#define PP_CORE_TRACE(...) ::Pepper::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define PP_CORE_INFO(...) ::Pepper::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define PP_CORE_WARN(...) ::Pepper::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define PP_CORE_ERROR(...) ::Pepper::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define PP_CORE_CRITICAL(...) ::Pepper::Log::GetCoreLogger()->critical(__VA_ARGS__)
-
-// Client log macros
-#define PP_TRACE(...) ::Pepper::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define PP_INFO(...) ::Pepper::Log::GetClientLogger()->info(__VA_ARGS__)
-#define PP_WARN(...) ::Pepper::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define PP_ERROR(...) ::Pepper::Log::GetClientLogger()->error(__VA_ARGS__)
-#define PP_CRITICAL(...) ::Pepper::Log::GetClientLogger()->critical(__VA_ARGS__)
+#ifdef PP_CORE
+  // Core log macros
+  #define PP_CORE_TRACE(...) ::Pepper::Log::GetCoreLogger()->trace(__VA_ARGS__)
+  #define PP_CORE_INFO(...) ::Pepper::Log::GetCoreLogger()->info(__VA_ARGS__)
+  #define PP_CORE_WARN(...) ::Pepper::Log::GetCoreLogger()->warn(__VA_ARGS__)
+  #define PP_CORE_ERROR(...) ::Pepper::Log::GetCoreLogger()->error(__VA_ARGS__)
+  #define PP_CORE_CRITICAL(...) ::Pepper::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#else
+  // Client log macros
+  #define PP_TRACE(...) ::Pepper::Log::GetClientLogger()->trace(__VA_ARGS__)
+  #define PP_INFO(...) ::Pepper::Log::GetClientLogger()->info(__VA_ARGS__)
+  #define PP_WARN(...) ::Pepper::Log::GetClientLogger()->warn(__VA_ARGS__)
+  #define PP_ERROR(...) ::Pepper::Log::GetClientLogger()->error(__VA_ARGS__)
+  #define PP_CRITICAL(...) ::Pepper::Log::GetClientLogger()->critical(__VA_ARGS__)
+#endif
