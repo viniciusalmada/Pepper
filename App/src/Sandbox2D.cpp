7c73474827c4c8ce2f801cfcb209ce1b6e22791a
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-10 13:05:06 -0300

    move draw data to Renderer2D

--- App/src/Sandbox2D.cpp
+++ App/src/Sandbox2D.cpp
@@ -6,40 +6,39 @@
 
 #include "IO.hpp"
 
-#include <imgui.h>
 #include <glm/gtc/type_ptr.hpp>
+#include <imgui.h>
 
-Sandbox2D::Sandbox2D() : Layer("Sandbox 2D"), camera_controller(1280.0f / 720.0f, true), square_color({0.2f, 0.3f, 0.8f})
+Sandbox2D::Sandbox2D() :
+    Layer("Sandbox 2D"),
+    camera_controller(1280.0f / 720.0f, true),
+    square_color({ 0.2f, 0.3f, 0.8f })
 {
 }
 
 void Sandbox2D::OnAttach()
 {
-  vertex_array = Pepper::VertexArray::Create();
-
-  // clang-format off
-  std::vector<float> square_vertices = {
-    -0.5f, -0.5f, -0.0f,
-     0.5f, -0.5f, -0.0f,
-     0.5f,  0.5f, -0.0f,
-    -0.5f,  0.5f, -0.0f,
-  };
-  // clang-format on
-
-  auto square_vb = Pepper::VertexBuffer::Create(square_vertices, vertex_array->GetRendererID());
-  square_vb->SetLayout({ { Pepper::ShaderDataType::Float3, "in_position" } });
-  vertex_array->AddVertexBuffer(square_vb);
-
-  std::vector<uint32_t> square_indices = { 0, 1, 2, 2, 3, 0 };
-  auto square_ib = Pepper::IndexBuffer::Create(square_indices, vertex_array->GetRendererID());
-  vertex_array->SetIndexBuffer(square_ib);
-
-  shader = Pepper::Shader::Create(IO::GetAssets() / "shaders/FlatColor");
+  Pepper::Renderer2D::Init();
 }
 
-void Sandbox2D::OnDetach() {}
+void Sandbox2D::OnDetach()
+{
+  Pepper::Renderer2D::Shutdown();
+}
 
-void Sandbox2D::OnUpdate(Pepper::TimeStep) {}
+void Sandbox2D::OnUpdate(Pepper::TimeStep ts)
+{
+  camera_controller.OnUpdate(ts);
+
+  Pepper::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+  Pepper::RenderCommand::Clear();
+
+  Pepper::Renderer2D::BeginScene(camera_controller.GetCamera());
+
+  Pepper::Renderer2D::DrawQuad({ -0.5f, -0.5f }, { 0.2, 0.2 }, { 1.0f, 0.2f, 0.3f, 1.0f });
+
+  Pepper::Renderer2D::EndScene();
+}
 
 void Sandbox2D::OnImGuiRender()
 {
