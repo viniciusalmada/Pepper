543d307f9f65d119ad757641e5c3941f4eaf384b
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-10 13:05:23 -0300

    adjust shader to use vec4 in uniform color

--- App/assets/shaders/FlatColor.glsl
+++ App/assets/shaders/FlatColor.glsl
@@ -6,13 +6,19 @@ layout(location = 0) in vec3 in_position;
 uniform mat4 u_view_projection;
 uniform mat4 u_transform;
 
-void main() { gl_Position = u_view_projection * u_transform * vec4(in_position, 1.0); }
+void main()
+{
+  gl_Position = u_view_projection * u_transform * vec4(in_position, 1.0);
+}
 
 //##fragment
 #version 330 core
 
 out vec4 out_color;
 
-uniform vec3 u_color;
+uniform vec4 u_color;
 
-void main() { out_color = vec4(u_color, 1.0); }
+void main()
+{
+  out_color = u_color;
+}
