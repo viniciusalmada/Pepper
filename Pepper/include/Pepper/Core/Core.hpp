1293da2e25bcdc9cf37e3ddbd4d99a40e55027bc
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-10 13:07:00 -0300

    add spaces in pointers creators

--- Pepper/include/Pepper/Core/Core.hpp
+++ Pepper/include/Pepper/Core/Core.hpp
@@ -36,6 +36,7 @@ namespace Pepper
 {
   template <typename T>
   using Scope = std::unique_ptr<T>;
+
   template <typename T, typename... Args>
   constexpr Scope<T> CreateScope(Args&&... args)
   {
@@ -44,6 +45,7 @@ namespace Pepper
 
   template <typename T>
   using Ref = std::shared_ptr<T>;
+  
   template <typename T, typename... Args>
   constexpr Ref<T> CreateRef(Args&&... args)
   {
