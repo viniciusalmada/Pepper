81b407e5c41df3580af0ee1c6d32f97b3da85b19
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-17 02:25:13 -0300

    add more stl headers to pch

--- Pepper/include/PepperPCH.hpp
+++ Pepper/include/PepperPCH.hpp
@@ -1,23 +1,26 @@
 #pragma once
 
-// clang-format off
 #include <algorithm>
 #include <any>
 #include <chrono>
 #include <cstdint>
+#include <filesystem>
 #include <fstream>
 #include <functional>
-#include <filesystem>
 #include <iostream>
+#include <map>
 #include <memory>
+#include <mutex>
 #include <ranges>
 #include <sstream>
 #include <string>
+#include <thread>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
 #include <vector>
 
+// clang-format off
 #include "Pepper/Core/Log.hpp"
 
 #ifdef PP_PLATFORM_WINDOWS
