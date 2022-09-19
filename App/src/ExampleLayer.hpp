768f011acf8f9c2513506dbd9beb361eaeb53bcc
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-19 01:55:00 -0300

    fix initialization order of ExampleLayer

--- App/src/ExampleLayer.hpp
+++ App/src/ExampleLayer.hpp
@@ -14,21 +14,20 @@ public:
   void OnEvent(Pepper::Event&) override;
 
 private:
+  Pepper::OrthoCameraController camera_controller;
+  glm::vec3 square_position;
+  glm::vec4 square_color;
+
+  Pepper::Ref<Pepper::VertexArray> triangle_VAO;
+  Pepper::Ref<Pepper::VertexArray> square_VAO;
+
   // Pepper::Ref<Pepper::Shader> shader;
   // Pepper::Ref<Pepper::Shader> flat_color_shader;
   // Pepper::Ref<Pepper::Shader> texture_shader;
   Pepper::ShaderLibrary shader_library;
 
-  Pepper::Ref<Pepper::VertexArray> triangle_VAO;
-  Pepper::Ref<Pepper::VertexArray> square_VAO;
-
   Pepper::Ref<Pepper::Texture2D> texture;
   Pepper::Ref<Pepper::Texture2D> pepper_texture;
 
-  Pepper::OrthoCameraController camera_controller;
-
   const float SQUARE_MOVE_SPEED = 1.0f;
-
-  glm::vec3 square_position;
-  glm::vec4 square_color;
 };
