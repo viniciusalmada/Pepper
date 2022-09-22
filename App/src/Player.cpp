82fc2445df5f48fafb9b1675b370746853b07e49
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-22 16:46:03 -0300

    add flames animation to rocket

--- App/src/Player.cpp
+++ App/src/Player.cpp
@@ -4,17 +4,26 @@
 
 #include "Player.hpp"
 
+#include "Utilities.hpp"
+
 #include <imgui.h>
 
 Player::Player() {}
 
 void Player::LoadAssets()
 {
-  m_texture = Pepper::Texture2D::Create("assets/textures/rocket.png");
+  m_rocket_tex = Pepper::Texture2D::Create("assets/textures/rocket.png");
+  m_flame1_tex = Pepper::Texture2D::Create("assets/textures/flame1.png");
+  m_flame2_tex = Pepper::Texture2D::Create("assets/textures/flame2.png");
+  m_flame3_tex = Pepper::Texture2D::Create("assets/textures/flame3.png");
+  m_flame_tex = m_flame1_tex;
 }
 
 void Player::OnUpdate(Pepper::TimeStep ts)
 {
+  if (!m_move)
+    return;
+
   if (Pepper::Input::IsKeyPressed(PP_KEY_SPACE))
   {
     m_velocity.y += m_engine_power;
@@ -26,14 +35,28 @@ void Player::OnUpdate(Pepper::TimeStep ts)
     m_rotation_deg -= 2.5f;
   }
   m_rotation_deg = std::clamp(m_rotation_deg, -180.0f, 0.0f);
+  m_flame_alpha = Utils::Interpol(-20.0f, 20.0f, 0.0, 1.0f, m_velocity.y);
+  if (m_flame_alpha == std::clamp(m_flame_alpha, 0.0f, 0.3f))
+  {
+    m_flame_tex = m_flame1_tex;
+  }
+  else if (m_flame_alpha == std::clamp(m_flame_alpha, 0.3f, 0.6f))
+  {
+    m_flame_tex = m_flame2_tex;
+  }
+  else
+  {
+    m_flame_tex = m_flame3_tex;
+  }
   m_velocity.y = std::clamp(m_velocity.y, -20.0f, 20.0f);
   m_position += m_velocity * (float)ts;
 
-  m_position.y = std::clamp(m_position.y, -5.625f - m_size.y, 5.625f + m_size.y);
+  m_position.y = std::clamp(m_position.y, -5.625f - m_rocket_size.y, 5.625f + m_rocket_size.y);
 }
 
 void Player::OnImGuiLayer()
 {
+  ImGui::Checkbox("Move rocket", &m_move);
   ImGui::DragFloat("Engine Power", &m_engine_power, 0.05f, 0.05f, 20.0f);
   ImGui::DragFloat("Gravity", &m_gravity, 0.05f, 0.05f, 20.0f);
   ImGui::DragFloat("Rotation", &m_rotation_deg, 1.f, -180.f, 0.0f);
@@ -41,5 +64,11 @@ void Player::OnImGuiLayer()
 
 void Player::OnRendererCall()
 {
-  Pepper::Renderer2D::DrawRotatedQuad(m_position, m_size, m_rotation_deg, m_texture);
+  Pepper::Renderer2D::DrawRotatedQuad(m_position, m_rocket_size, m_rotation_deg, m_rocket_tex);
+  Pepper::Renderer2D::DrawRotatedQuad({ m_position.x, m_position.y, 0.1 },
+                                      m_flames_size,
+                                      m_rotation_deg,
+                                      m_flame_tex,
+                                      1.0f,
+                                      { 1.0f, 1.0f, 1.0f, m_flame_alpha });
 }
--- App/src/Player.hpp
+++ App/src/Player.hpp
@@ -20,10 +20,17 @@ private:
   glm::vec2 m_position = { 0.0f, 0.0f };
   glm::vec2 m_velocity = { 0.0f, 0.0f };
   float m_rotation_deg = 0.0f;
+  float m_flame_alpha = 1.0f;
+  bool m_move = true;
 
-  Pepper::Ref<Pepper::Texture2D> m_texture;
-
+  Pepper::Ref<Pepper::Texture2D> m_rocket_tex;
+  Pepper::Ref<Pepper::Texture2D> m_flame_tex;
+  Pepper::Ref<Pepper::Texture2D> m_flame1_tex;
+  Pepper::Ref<Pepper::Texture2D> m_flame2_tex;
+  Pepper::Ref<Pepper::Texture2D> m_flame3_tex;
+  
   float m_engine_power = 0.5f;
   float m_gravity = 0.4f;
-  glm::vec2 m_size = { 0.25f, 0.46875f };
+  glm::vec2 m_rocket_size = { 0.25f, 0.46875f };
+  glm::vec2 m_flames_size = { 0.25f, 0.96875f };
 };
