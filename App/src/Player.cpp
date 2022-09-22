83432ffc75accb2246fe3e73251c13d9b121b834
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-22 14:31:56 -0300

    set player movement and rotation

--- App/src/Player.cpp
+++ App/src/Player.cpp
@@ -18,22 +18,28 @@ void Player::OnUpdate(Pepper::TimeStep ts)
   if (Pepper::Input::IsKeyPressed(PP_KEY_SPACE))
   {
     m_velocity.y += m_engine_power;
+    m_rotation_deg += 3.5f;
   }
   else
   {
     m_velocity.y -= m_gravity;
+    m_rotation_deg -= 2.5f;
   }
-  m_velocity.y = glm::clamp(m_velocity.y, -20.0f, 20.0f);
+  m_rotation_deg = std::clamp(m_rotation_deg, -180.0f, 0.0f);
+  m_velocity.y = std::clamp(m_velocity.y, -20.0f, 20.0f);
   m_position += m_velocity * (float)ts;
+
+  m_position.y = std::clamp(m_position.y, -5.625f - m_size.y, 5.625f + m_size.y);
 }
 
 void Player::OnImGuiLayer()
 {
-  ImGui::DragFloat("Engine Power", &m_engine_power, 0.1f);
-  ImGui::DragFloat("Gravity", &m_gravity, 0.1f);
+  ImGui::DragFloat("Engine Power", &m_engine_power, 0.05f, 0.05f, 20.0f);
+  ImGui::DragFloat("Gravity", &m_gravity, 0.05f, 0.05f, 20.0f);
+  ImGui::DragFloat("Rotation", &m_rotation_deg, 1.f, -180.f, 0.0f);
 }
 
 void Player::OnRendererCall()
 {
-  Pepper::Renderer2D::DrawQuad(m_position, { 1.0f, 1.875f }, m_texture);
+  Pepper::Renderer2D::DrawRotatedQuad(m_position, m_size, m_rotation_deg, m_texture);
 }
