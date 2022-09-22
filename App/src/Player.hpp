c3f4ce8548ff7563666f35a37360fef79505d948
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-22 14:31:56 -0300

    set player movement and rotation

--- App/src/Player.hpp
+++ App/src/Player.hpp
@@ -19,9 +19,11 @@ public:
 private:
   glm::vec2 m_position = { 0.0f, 0.0f };
   glm::vec2 m_velocity = { 0.0f, 0.0f };
+  float m_rotation_deg = 0.0f;
 
   Pepper::Ref<Pepper::Texture2D> m_texture;
 
-  float m_engine_power = 0.1f;
-  float m_gravity = 0.8f;
+  float m_engine_power = 0.5f;
+  float m_gravity = 0.4f;
+  glm::vec2 m_size = { 0.25f, 0.46875f };
 };
