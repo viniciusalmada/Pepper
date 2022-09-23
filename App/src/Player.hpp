3407ae3be16d241550a0d9bb12eaaca070119743
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-23 12:47:50 -0300

    change engine increment to be based on timestep

--- App/src/Player.hpp
+++ App/src/Player.hpp
@@ -23,7 +23,7 @@ private:
   glm::vec2 m_velocity = { 2.0f, 0.0f };
   float m_rotation_deg = 0.0f;
   float m_flame_alpha = 1.0f;
-  bool m_move = false;
+  bool m_move = false; 
 
   Pepper::Ref<Pepper::Texture2D> m_rocket_tex;
   Pepper::Ref<Pepper::Texture2D> m_flame_tex;
@@ -31,8 +31,8 @@ private:
   Pepper::Ref<Pepper::Texture2D> m_flame2_tex;
   Pepper::Ref<Pepper::Texture2D> m_flame3_tex;
   
-  float m_engine_power = 0.5f;
-  float m_gravity = 0.4f;
+  float m_engine_power = 30.0f;
+  float m_gravity = 24.0f;
   glm::vec2 m_rocket_size = { 0.5f, 0.875f };
   glm::vec2 m_flames_size = { 0.5f, 2.0f };
 };
