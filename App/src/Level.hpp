630716de2e3b958b11d5a409aa33b960ad41ed40
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-23 00:07:06 -0300

    add draw of obstacles

--- App/src/Level.hpp
+++ App/src/Level.hpp
@@ -2,6 +2,12 @@
 
 #include "Player.hpp"
 
+struct Obstacle
+{
+  glm::vec3 m_position{ 0.0f, 0.0f, 0.0f };
+  glm::vec2 m_size{ 1.0, 4.0f };
+};
+
 class Level
 {
 public:
@@ -10,7 +16,7 @@ public:
   void Init();
 
   void OnUpdate(Pepper::TimeStep);
-  
+
   void OnImGuiRender();
 
   void OnRendererCall();
@@ -18,5 +24,11 @@ public:
   const glm::vec2& GetPlayerPosition() const;
 
 private:
+  void UpdateObstacle(Obstacle& obs);
+
   Player m_player;
+  std::array<Obstacle, 5> m_obstacles;
+  float m_obs_x_pos = 5.0f;
+
+  static const float OBSTACLE_HIDDEN;
 };
