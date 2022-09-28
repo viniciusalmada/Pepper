9d7e63180a77c6e271398d67e84fd8a0931fca6f
Vinicius Almada <viniciusalmada10@gmail.com>
2022-09-28 00:52:35 -0300

    create a collision detector between circle and line

--- App/src/Utilities.cpp
+++ App/src/Utilities.cpp
@@ -25,27 +25,50 @@ float Utils::Interpol(float srcMin, float srcMax, float tarMin, float tarMax, fl
 }
 
 /// @brief Checks if a line intersect a circle (https://mathworld.wolfram.com/Circle-LineIntersection.html)
-bool Utils::LineIntersectCircle(const glm::vec2& p1, const glm::vec2& p2, float radius, const glm::vec2& center)
+std::tuple<bool, glm::vec2>
+Utils::LineIntersectCircle(const glm::vec2& p1, const glm::vec2& p2, float radius, const glm::vec2& center)
 {
   if (p1 == p2)
-    return false;
+    return { false, {} };
+
+  const float dist_p1 = glm::distance(p1, center);
+  if (dist_p1 <= radius)
+    return { true, p1 };
+  const float dist_p2 = glm::distance(p2, center);
+  if (dist_p2 <= radius)
+    return { true, p2 };
 
   glm::vec2 new_p1 = p1 - center;
   glm::vec2 new_p2 = p2 - center;
 
   glm::vec2 d = new_p2 - new_p1;
-  float dr = glm::length(d);
-  float d_det = new_p1.x * new_p2.y - new_p2.x * new_p1.y;
+  float d_r = glm::length(d);
+  float d_big = new_p1.x * new_p2.y - new_p2.x * new_p1.y;
 
-  float discriminant = std::pow(radius, 2.0f) * std::pow(dr, 2.0f) - std::pow(d_det, 2.0f);
+  float discriminant = std::pow(radius, 2.0f) * std::pow(d_r, 2.0f) - std::pow(d_big, 2.0f);
   if (discriminant < 0.0f)
-    return false;
+    return { false, {} };
 
-  float dist_p1 = glm::distance(p1, center);
-  float dist_p2 = glm::distance(p2, center);
+  auto sig = [](float x)
+  {
+    return x < 0.0f ? -1 : 1;
+  };
 
   if (dist_p1 <= radius || dist_p2 <= radius)
-    return true;
+  {
+    glm::vec2 s_intersection_pt;
+    s_intersection_pt.x = (d_big * d.y + sig(d.y) * d.x * std::sqrtf(discriminant)) / (d_r * d_r);
+    s_intersection_pt.y = (-d_big * d.x + std::abs(d.y) * std::sqrtf(discriminant)) / (d_r * d_r);
 
-  return false;
+    float dist_1 = glm::distance(s_intersection_pt, new_p1);
+    float dist_2 = glm::distance(s_intersection_pt, new_p2);
+
+    if (dist_1 + dist_2 <= d_r)
+    {
+      s_intersection_pt += center;
+      return { true, s_intersection_pt };
+    }
+  }
+
+  return { false, {} };
 }
--- App/src/Utilities.hpp
+++ App/src/Utilities.hpp
@@ -5,5 +5,6 @@ namespace Utils
 {
   float Interpol(float srcMin, float srcMax, float tarMin, float tarMax, float ref);
 
-  bool LineIntersectCircle(const glm::vec2& p1, const glm::vec2& p2, float radius, const glm::vec2& center);
+  std::tuple<bool, glm::vec2>
+  LineIntersectCircle(const glm::vec2& p1, const glm::vec2& p2, float radius, const glm::vec2& center);
 }
