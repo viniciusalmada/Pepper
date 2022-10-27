1d3f8ebe7b412c9a76fa3e0e70ae406f49c8fd52
Vinicius Almada <viniciusalmada10@gmail.com>
2022-10-26 23:51:02 -0300

    fixed Random uniform distributions

--- App/src/Random.cpp
+++ App/src/Random.cpp
@@ -5,16 +5,22 @@ namespace
   std::mt19937 s_random_engine{ std::random_device{}() };
 }
 
+std::uniform_int_distribution<int32_t> Random::s_int32_distribution{};
+std::uniform_real_distribution<float> Random::s_float_distribution{};
+
+using IntParam = std::uniform_int_distribution<int32_t>::param_type;
+using FloatParam = std::uniform_real_distribution<float>::param_type;
+
 int32_t Random::Int(int32_t min, int32_t max)
 {
-  std::uniform_int_distribution<int32_t> distribution{ min, max };
+  s_int32_distribution.param(IntParam(min, max));
 
-  return distribution(s_random_engine);
+  return s_int32_distribution(s_random_engine);
 }
 
 float Random::Float(float min, float max)
 {
-  std::uniform_real_distribution<float> distribution{ min, max };
+  s_float_distribution.param(FloatParam(min, max));
 
-  return distribution(s_random_engine);
+  return s_float_distribution(s_random_engine);
 }
--- App/src/Random.hpp
+++ App/src/Random.hpp
@@ -8,5 +8,10 @@ public:
   static int32_t Int(int32_t min = std::numeric_limits<int32_t>::min(),
                      int32_t max = std::numeric_limits<int32_t>::max());
 
-  static float Float(float min = std::numeric_limits<float>::min(), float max = std::numeric_limits<float>::max());
+  static float Float(float min = std::numeric_limits<float>::min(),
+                     float max = std::numeric_limits<float>::max());
+
+private:
+  static std::uniform_int_distribution<int32_t> s_int32_distribution;
+  static std::uniform_real_distribution<float> s_float_distribution;
 };
